4cfb3bf1fb335219333d879411c162ae2062777a
vrajeshjayswal <vrajeshjayswal@gmail.com>
2020-04-30 15:06:28 +0530

    first commit

---
+++ src/test/java/com/example/SpringRestAPIDemo/SpringRestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringRestAPIDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
